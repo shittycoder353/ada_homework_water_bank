105b3299642d5c857aebbf8ee61137982b7a33df
miso <mischo0055@pobox.sk>
2015-04-29 12:52:32 +0200

    Messages: showconsole => server Nova vyska hladiny

--- showconsole/src/showconsole.adb
+++ showconsole/src/showconsole.adb
@@ -6,10 +6,12 @@ with Client_Msgs;
 with ValueTypes;			use ValueTypes;
 with Ada.Text_IO;			use Ada.Text_IO;
 with GNAT.OS_Lib;
+with Ada.Integer_Text_IO; use  Ada.Integer_Text_IO;
 
 procedure ShowConsole is
-  c : Connection.TConnectionRef;
-  bConnectionWasTerminated : Boolean;
+   c : Connection.TConnectionRef;
+   bConnectionWasTerminated : Boolean;
+   newSurfaceValue : Integer :=0;
 begin
   Connection.GlobalInit;
   --
@@ -33,8 +35,17 @@ begin
     end;
     --
     loop
-
-      --delay 1000.0;
+         Put_Line("Enter new height of water level:");
+         get(newSurfaceValue);
+         declare
+            use Client_Msgs;
+            msg_CPtr : CSetValue_CPtr := new CSetValue;
+         begin
+            msg_CPtr.valueName := ValueName_Pkg.To_Bounded_String("Zadana vyska"); --vyska hladiny zadana
+            msg_CPtr.value := validValue;
+            msg_CPtr.value.value := Long_Float(newSurfaceValue);
+            Connection.SendMessage(c, CMessage_CPtr(msg_CPtr), bConnectionWasTerminated);
+         end;
     end loop;
     --
     Connection.Disconnect(c);
